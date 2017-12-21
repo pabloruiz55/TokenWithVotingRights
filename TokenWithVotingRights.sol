1062a188fa9d75af30d728cefb91f411f6d4e252
Pablo Ruiz <pabloruiz55@gmail.com>
2017-12-21 12:11:27 -0300

    Create TokenWithVotingRights.sol

---
+++ TokenWithVotingRights.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.19;
+
+contract TokenWithVotingRights is ERC20Basic {
+    mapping(address => uint256) public votingRights;
+    
+    event TransferVotingRights(address indexed from, address indexed to, uint256 value);
+
+    function TokenWithVotingRights() public {
+        totalSupply = 100000 * 10 ** uint(decimals);
+        balances[msg.sender] = totalSupply;
+        votingRights[msg.sender] = totalSupply;
+    }
+    
+    function transferTokensAndVotingRights(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        
+        transferVotingRights(_to, _value);
+        
+        return(super.transfer(_to, _value));
+    }
+    
+    function transferVotingRights(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0));
+        
+        uint votesToTransfer = _value;
+        if(_value > votingRights[msg.sender])
+            votesToTransfer = votingRights[msg.sender];
+    
+        // SafeMath.sub will throw if there is not enough balance.
+        votingRights[msg.sender] = votingRights[msg.sender].sub(votesToTransfer);
+        votingRights[_to] = votingRights[_to].add(votesToTransfer);
+        TransferVotingRights(msg.sender, _to, votesToTransfer);
+        return true;
+    }
+    
+    function votingRightsOf(address _owner) public view returns (uint256) {
+        return ((votingRights[_owner] <= balances[_owner]) ? votingRights[_owner] : balances[_owner]);
+    }
+}
